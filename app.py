29886aeb1537ebe0be92d6616c5eb9cbf821a4dc
air15 <air15@air15ui-MacBookAir.local>
2026-07-25 14:50:02 +0900

    fix pem encoding error completely

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import streamlit as st
 import gspread
+from google.oauth2.service_account import Credentials
 
 @st.cache_resource
 def init_google_sheets():
-    gc = gspread.service_account_from_dict(dict(st.secrets))
+    scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+    
+    # 시크릿 정보를 복사한 뒤 private_key의 이스케이프 문자열을 진짜 줄바꿈으로 변환
+    creds_dict = dict(st.secrets)
+    if "private_key" in creds_dict:
+        # 백슬래시 n이 문자로 들어온 경우 실제 개행 문자로 강제 치환
+        creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
+        
+    creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
+    gc = gspread.authorize(creds)
     sh = gc.open("SmartTimerDB")
     return sh
 
